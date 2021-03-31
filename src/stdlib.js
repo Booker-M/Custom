742c42240b3d4e3cd15cc45c69144875d9bd6e49
igreen1 <iangreen2.00@gmail.com>
2021-03-30 23:03:50 -0700

    Linting on standard library to account for changes from Toal's carlos-lang

--- src/stdlib.js
+++ src/stdlib.js
@@ -1,25 +1,14 @@
-import { Type, FunctionType, Variable, Function, ArrayType } from "./ast.js";
+import { Type, FunctionType, Function, ArrayType } from "./ast.js";
 import fs from "fs";
 
 const languageConfig = JSON.parse(
   fs.readFileSync("./config/customConfig.json", "utf8")
 );
 
-// function makeConstant(name, type, value) {
-//   return Object.assign(new Variable(name, true), { type, value });
-// }
 
 function makeFunction(name, type) {
   return Object.assign(new Function(name), { type });
 }
-
-const floatsType = new ArrayType(Type.FLOAT);
-const floatFloatType = new FunctionType([Type.FLOAT], Type.FLOAT);
-const floatFloatFloatType = new FunctionType(
-  [Type.FLOAT, Type.FLOAT],
-  Type.FLOAT
-);
-const stringToIntsType = new FunctionType([Type.STRING], floatsType);
 
 const types = {};
 types[languageConfig.int] = Type.INT;
