8c59e6604a33dda48909a092f93412efa3af57d2
Booker-M <bookerjmartin@gmail.com>
2021-04-12 16:54:55 -0700

    Moved compiler.js functionality into custom.js (like in Carlos)

--- src/custom.js
+++ src/custom.js
@@ -1,9 +1,14 @@
 #! /usr/bin/env node
 
 import fs from "fs/promises";
-import process from "process";
-import compile from "./compiler.js";
 import fsNonPromise from "fs";
+import util from "util";
+import process from "process";
+import { Program } from "./ast.js";
+import parse from "./parser.js";
+import analyze from "./analyzer.js";
+// import optimize from "./optimizer.js";
+import generate from "./generator.js";
 
 const languageConfig = JSON.parse(
   fsNonPromise.readFileSync("./config/customConfig.json", "utf8")
@@ -23,6 +28,23 @@ Prints to stdout according to <outputType>, which must be one of:
   llvm       the translation to LLVM
   string     the string with any string literals replaced with your custom config values
 `;
+
+function compile(source, outputType) {
+  outputType = outputType.toLowerCase();
+  if (outputType == "string") {
+    return source;
+  } else if (outputType === "ast") {
+    return parse(source);
+  } else if (outputType === "analyzed") {
+    return analyze(parse(source));
+    // } else if (outputType === "optimized") {
+    //   return optimize(analyze(parse(source)));
+  } else if (outputType === "js") {
+    return generate(optimize(analyze(parse(source))));
+  } else {
+    return "Unknown output type";
+  }
+}
 
 async function compileFromFile(filename, outputType) {
   try {
---
+++ src/generator.js
@@ -0,0 +1,201 @@
+// Code Generator Carlos -> JavaScript
+//
+// Invoke generate(program) with the program node to get back the JavaScript
+// translation as a string.
+
+import { StatementIfElse, Type, ArrayType, SetType, DictType } from "./ast.js";
+import * as stdlib from "./stdlib.js";
+
+export default function generate(program) {
+  const output = [];
+
+  const standardFunctions = new Map([
+    [stdlib.functions.print, x => `console.log(${x})`],
+    [stdlib.functions.sin, x => `Math.sin(${x})`],
+    [stdlib.functions.cos, x => `Math.cos(${x})`],
+    [stdlib.functions.exp, x => `Math.exp(${x})`],
+    [stdlib.functions.ln, x => `Math.log(${x})`],
+    [stdlib.functions.hypot, (x, y) => `Math.hypot(${x},${y})`],
+    [stdlib.functions.bytes, s => `[...Buffer.from(${s}, "utf8")]`],
+    // TODO CODEPOINTS
+  ]);
+
+  // Variable and function names in JS will be suffixed with _1, _2, _3,
+  // etc. This is because "switch", for example, is a legal name in Carlos,
+  // but not in JS. So we want to generate something like "switch_1".
+  // We handle this by mapping each name to its suffix.
+  const targetName = (mapping => {
+    return entity => {
+      if (!mapping.has(entity)) {
+        mapping.set(entity, mapping.size + 1);
+      }
+      return `${entity.name ?? entity.description}_${mapping.get(entity)}`;
+    };
+  })(new Map());
+
+  const gen = node => generators[node.constructor.name](node);
+
+  const generators = {
+    Program(p) {
+      gen(p.statements);
+    },
+    VariableDeclaration(d) {
+      // We don't care about const vs. let in the generated code. The analyzer
+      // has already checked we never wrote to a const, so let is always fine.
+      output.push(`let ${gen(d.variable)} = ${gen(d.initializer)};`);
+    },
+    TypeDeclaration(d) {
+      output.push(`class ${gen(d.type)} {`);
+      output.push(`constructor(${gen(d.type.fields).join(",")}) {`);
+      for (let field of d.type.fields) {
+        output.push(`this[${JSON.stringify(gen(field))}] = ${gen(field)};`);
+      }
+      output.push("}");
+      output.push("}");
+    },
+    ArrayType(t) {
+      return targetName(t);
+    },
+    SetType(t) {
+      return targetName(t);
+    },
+    DictType(t) {
+      return targetName(t);
+    },
+    Field(f) {
+      return targetName(f);
+    },
+    FunctionDeclaration(d) {
+      output.push(
+        `function ${gen(d.fun)}(${gen(d.fun.parameters).join(", ")}) {`
+      );
+      gen(d.body);
+      output.push("}");
+    },
+    Parameter(p) {
+      return targetName(p);
+    },
+    Variable(v) {
+      if (v === stdlib.constants.π) {
+        return "Math.PI";
+      }
+      return targetName(v);
+    },
+    Function(f) {
+      return targetName(f);
+    },
+    Increment(s) {
+      output.push(`${gen(s.variable)}++;`);
+    },
+    Decrement(s) {
+      output.push(`${gen(s.variable)}--;`);
+    },
+    Assignment(s) {
+      output.push(`${gen(s.target)} = ${gen(s.source)};`);
+    },
+    BreakStatement(s) {
+      output.push("break;");
+    },
+    ReturnStatement(s) {
+      output.push(`return ${gen(s.expression)};`);
+    },
+    ShortReturnStatement(s) {
+      output.push("return;");
+    },
+    StatementIfElse(s) {
+      output.push(`if (${gen(s.test)}) {`);
+      gen(s.consequent);
+      if (s.alternate.constructor === StatementIfElse) {
+        output.push("} else");
+        gen(s.alternate);
+      } else {
+        output.push("} else {");
+        gen(s.alternate);
+        output.push("}");
+      }
+    },
+    WhileStatement(s) {
+      output.push(`while (${gen(s.test)}) {`);
+      gen(s.body);
+      output.push("}");
+    },
+    RepeatStatement(s) {
+      const i = targetName({ name: "i" });
+      output.push(`for (let ${i} = 0; ${i} < ${gen(s.count)}; ${i}++) {`);
+      gen(s.body);
+      output.push("}");
+    },
+    ForRangeStatement(s) {
+      const i = targetName(s.iterator);
+      const op = s.op === "..." ? "<=" : "<";
+      output.push(
+        `for (let ${i} = ${gen(s.low)}; ${i} ${op} ${gen(s.high)}; ${i}++) {`
+      );
+      gen(s.body);
+      output.push("}");
+    },
+    ForStatement(s) {
+      output.push(`for (let ${gen(s.iterator)} of ${gen(s.collection)}) {`);
+      gen(s.body);
+      output.push("}");
+    },
+    Conditional(e) {
+      return `((${gen(e.test)}) ? (${gen(e.consequent)}) : (${gen(
+        e.alternate
+      )}))`;
+    },
+    BinaryExpression(e) {
+      const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op;
+      return `(${gen(e.left)} ${op} ${gen(e.right)})`;
+    },
+    UnaryExpression(e) {
+      return `${e.op}(${gen(e.operand)})`;
+    },
+    EmptyOptional(e) {
+      return "undefined";
+    },
+    SubscriptExpression(e) {
+      return `${gen(e.array)}[${gen(e.index)}]`;
+    },
+    ArrayExpression(e) {
+      return `[${gen(e.elements).join(",")}]`;
+    },
+    EmptyArray(e) {
+      return "[]";
+    },
+    MemberExpression(e) {
+      return `(${gen(e.object)}[${JSON.stringify(gen(e.field))}])`;
+    },
+    Call(c) {
+      const targetCode = standardFunctions.has(c.callee)
+        ? standardFunctions.get(c.callee)(gen(c.args))
+        : c.callee.constructor === ArrayType ||
+          c.callee.constructor === SetType ||
+          c.callee.constructor === DictType
+        ? `new ${gen(c.callee)}(${gen(c.args).join(", ")})`
+        : `${gen(c.callee)}(${gen(c.args).join(", ")})`;
+      if (c.callee instanceof Type || c.callee.type.returnType !== Type.VOID) {
+        return targetCode;
+      }
+      output.push(`${targetCode};`);
+    },
+    Number(e) {
+      return e;
+    },
+    BigInt(e) {
+      return e;
+    },
+    Boolean(e) {
+      return e;
+    },
+    String(e) {
+      return JSON.stringify(e);
+    },
+    Array(a) {
+      return a.map(gen);
+    },
+  };
+
+  gen(program);
+  return output.join("\n");
+}
